07acdead446e671f4d5294a7d7d670ea0250e274
unknown <YY333@ccs.local>
2025-03-27 20:16:52 +0800

    fixed the names

--- routes/exam-group.js
+++ routes/exam-group.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();  
 
 router.get('/', (req, res) => {  
-  res.json({ "message": "Group <your_group> API" }); // Replace <your_group>  
+  res.json({ "message": "Group B API" });
 });  
 
 module.exports = router;  
